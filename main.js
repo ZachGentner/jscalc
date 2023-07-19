5dfc22e1db6977520c66497ffda72d7694eac2ff
Zach Gentner <zach.gentner@gmail.com>
2023-07-19 12:47:51 -0400

    Added additional functionality.

--- main.js
+++ main.js
@@ -2,48 +2,56 @@ const displayEl = document.getElementById("display");
 const clearEl = document.getElementById("clear");
 const expressionEl = document.getElementById("expression");
 
+const isNonZero = /[1-9]/;
+const isOperator = /[-+*÷]/;
+
 let expression = [];
 
 function btnPress(char){
     //if the button pressed is a number 1-9, add the number to the display.
     //if the button is an operator, clear the display and add the operator to the list of operands
-    const isNonZero = /[1-9]/;
-    const isOperator = /[-+*÷]/;
     
-//Test if the button pressed was a digit 1-9
-    if (isNonZero.test(char)){
-        if(displayEl.innerText === "0") {
+//NON-ZERO: Test if the button pressed was a digit 1-9
+    if (isNonZero.test(char)) {
+        if (displayEl.innerText === "0") {
             displayEl.innerText = char;
         } else {
             displayEl.innerText += char;
         }
     }
 
-    if (isOperator.test(char)){
-
-    }
-
-
-//Test if the button pressed was a zero.
+//ZERO: Test if the button pressed was a zero.
     if (char === 0) {
-        if(displayEl.innerText != "0"){
+        if (displayEl.innerText != "0"){
             displayEl.innerText += char;
         }
     }
 
-//Test if the button pressed was a decimal. User should not be able to enter a decimal if one already exists.
-    // if (char = ".") {
-    //     if(displayEl.innerText.search('.') == false) {
-    //         displayEl.innerText += char;
-    //     }
+//DECIMAL: Test if the button pressed was a decimal. User should not be able to enter a decimal if one already exists.
+    if (char === ".") {
+        if (displayEl.innerText.indexOf(".") === -1) {
+            displayEl.innerText += ".";
+        }
+    }
+
+
+//OPERATORS: Test if the button pressed was an operator. Add both the operand and the operator to the array. Update the expression and operator display.
+    if (isOperator.test(char) && isNonZero.test(displayEl.innerText)) {
+        updateExpression();
+        expression.push(displayEl.innerText);
+        expression.push(char);
+        expressionEl.innerText = expression.join(" ");
+        clearDisplay();
+    }
+
+
+//Test if the button pressed was equals. Evaluate expression.
+    // if (displayEl.innerText != "0") {
+    //     expression.push(displayEl.innerText);
     // }
-
-
-//Test if the button pressed was an operator.
-//Add both the operand and the operator to the array.
-//Update the expression display.
-
-//Test if the button pressed was equals. Evaluate expression
+    // if (isOperator.test(expression[expression.length - 1])) {
+    //     expression.pop();
+    // }
 }
 
 function clearDisplay(){
@@ -52,10 +60,10 @@ function clearDisplay(){
 
 function cancelExpression(){
     expression.length = 0;
+    expressionEl.innerText = ""
 }
 
 function updateExpression() {
-    console.log(expression.join(" "));
     expressionEl.innerText = expression.join(" ");
 }
 
