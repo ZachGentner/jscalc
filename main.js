e0d2a6503c4f6e2906f1580de6b735836a481c3b
Zach Gentner <zach.gentner@gmail.com>
2023-07-19 14:20:35 -0400

    Added event listener for keyboard usability

--- main.js
+++ main.js
@@ -7,6 +7,28 @@ const isOperator = /[-+*÷]/;
 
 let expression = [];
 
+// KEYBOARD FUNCTIONALITY: Add event listener for keyboard interactions.
+    window.addEventListener("keydown", function(event) {
+        if (event.key === "1") { btnPress(1); }
+        if (event.key === "2") { btnPress(2); }
+        if (event.key === "3") { btnPress(3); }
+        if (event.key === "4") { btnPress(4); }
+        if (event.key === "5") { btnPress(5); }
+        if (event.key === "6") { btnPress(6); }
+        if (event.key === "7") { btnPress(7); }
+        if (event.key === "8") { btnPress(8); }
+        if (event.key === "9") { btnPress(9); }
+        if (event.key === "0") { btnPress(0); }
+        if (event.key === ".") { btnPress("."); }
+        if (event.key === "+") { btnPress("+"); }
+        if (event.key === "-") { btnPress("-"); }
+        if (event.key === "*") { btnPress("*"); }
+        if (event.key === "/") { btnPress("÷"); }
+        if (event.ctrlKey && event.key === "c") { clearDisplay(); }
+        if (event.ctrlKey && event.key === "x") { cancelExpression(); }
+        if (event.key === "Enter") { evaluateExpression(); }
+        });
+
 function btnPress(char){
     //if the button pressed is a number 1-9, add the number to the display.
     //if the button is an operator, clear the display and add the operator to the list of operands
@@ -37,10 +59,9 @@ function btnPress(char){
 
 //OPERATORS: Test if the button pressed was an operator. Add both the operand and the operator to the array. Update the expression and operator display.
     if (isOperator.test(char) && isNonZero.test(displayEl.innerText)) {
-        updateExpression();
         expression.push(displayEl.innerText);
         expression.push(char);
-        expressionEl.innerText = expression.join(" ");
+        updateExpression();
         clearDisplay();
     }
 
@@ -70,5 +91,8 @@ function updateExpression() {
 function evaluateExpression() {
     //expression cannot be evaluated unless expression[expression.length-1] is equal to a numerical value.
     //expression must follow order of operations. MD AS
+    console.log("testing");
+
+
     return true;
 }
