c7bfa4c4497f05134a3e3722bab39f9acdb97580
Zach Gentner <zach.gentner@gmail.com>
2023-07-24 13:35:06 -0400

    Updated functionality

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 const displayEl = document.getElementById("display");
 const clearEl = document.getElementById("clear");
-const expressionEl = document.getElementById("expression");
 
 const isNonZero = /[1-9]/;
 const isOperator = /[-+*÷]/;
 
-let expression = [];
+let expression = "";
 
 // KEYBOARD FUNCTIONALITY: Add event listener for keyboard interactions.
     window.addEventListener("keydown", function(event) {
@@ -27,12 +26,13 @@ let expression = [];
         if (event.ctrlKey && event.key === "c") { clearDisplay(); }
         if (event.ctrlKey && event.key === "x") { cancelExpression(); }
         if (event.key === "Enter") { evaluateExpression(); }
+        if (event.key === "Backspace") { backspace(); }11
         });
 
 function btnPress(char){
     //if the button pressed is a number 1-9, add the number to the display.
     //if the button is an operator, clear the display and add the operator to the list of operands
-    
+
 //NON-ZERO: Test if the button pressed was a digit 1-9
     if (isNonZero.test(char)) {
         if (displayEl.innerText === "0") {
@@ -43,36 +43,48 @@ function btnPress(char){
     }
 
 //ZERO: Test if the button pressed was a zero.
-    if (char === 0) {
+    else if (char === 0) {
         if (displayEl.innerText != "0"){
             displayEl.innerText += char;
         }
     }
 
 //DECIMAL: Test if the button pressed was a decimal. User should not be able to enter a decimal if one already exists.
-    if (char === ".") {
+    else if (char === ".") {
         if (displayEl.innerText.indexOf(".") === -1) {
             displayEl.innerText += ".";
         }
     }
 
 
+//PERCENTAGE: Test if the button pressed was the percent operator.
+    if(char === "%") {
+        displayEl.innerHTML = 4;
+    }
+
+
+
 //OPERATORS: Test if the button pressed was an operator. Add both the operand and the operator to the array. Update the expression and operator display.
-    if (isOperator.test(char) && isNonZero.test(displayEl.innerText)) {
-        expression.push(displayEl.innerText);
-        expression.push(char);
-        updateExpression();
-        clearDisplay();
+    if (isOperator.test(char)) {
+        if (isOperator.test(displayEl.innerHTML.charAt(displayEl.innerHTML.length - 1))) {
+            backspace();
+            displayEl.innerHTML += char;
+        } else {
+            displayEl.innerHTML += char;
+        }
+        
     }
 
 
 //Test if the button pressed was equals. Evaluate expression.
-    // if (displayEl.innerText != "0") {
-    //     expression.push(displayEl.innerText);
-    // }
-    // if (isOperator.test(expression[expression.length - 1])) {
-    //     expression.pop();
-    // }
+    if(char === "=") { evaluateExpression(displayEl.innerHTML) }
+}
+
+function setOperator(char){
+    if (char === "+") { operator = "+"}
+    if (char === "-") { operator = "-"}
+    if (char === "*") { operator = "*"}
+    if (char === "÷") { operator = "÷"}
 }
 
 function clearDisplay(){
@@ -81,18 +93,35 @@ function clearDisplay(){
 
 function cancelExpression(){
     expression.length = 0;
-    expressionEl.innerText = ""
+    clearDisplay();
 }
 
-function updateExpression() {
-    expressionEl.innerText = expression.join(" ");
+function backspace(){
+    if(displayEl.innerHTML.length > 1) {
+        displayEl.innerHTML = displayEl.innerHTML.substring(0,displayEl.innerHTML.length - 1);
+    } else {
+        displayEl.innerHTML = 0;
+    }
 }
 
-function evaluateExpression() {
-    //expression cannot be evaluated unless expression[expression.length-1] is equal to a numerical value.
-    //expression must follow order of operations. MD AS
-    console.log("testing");
+function evaluateExpression(string) {
+    let result = 0;
 
+    if(isOperator.test(displayEl.innerHTML.charAt(displayEl.innerHTML.length - 1))) {
+        backspace();
+    }
+
+    for(let i = 0; i < displayEl.innerHTML.length; i++) {
+        if (isOperator.test(displayEl.innerHTML.charAt(i))) {
+            let operator = displayEl.innerHTML.charAt(i);
+            let operand1 = 0;
+            let operand2 = 0;
+
+            if (operator === "+") {
+                result = operand1 + operand2;
+            }
+        }
+    }
 
     return true;
 }
